cc2527cae7f034a791803cb9d5c6634481f4ded4
Jeison Rico <jeison.rico@utp.edu.co>
2023-10-23 14:43:25 -0500

    Introduccion

---
+++ 00_Introduccion.py
@@ -0,0 +1,22 @@
+#Esto es un comentario de una sola linea
+
+"""
+Esto es un comentario 
+de varias lineas
+"""
+
+'''
+Este tambien es un comentario
+en varias lineas
+'''
+
+#Imprimir un mensaje por consola (comillas dobles o simples)
+print("Hola Python")
+print('Hola Python')
+
+#Consultar el typo de dato
+print(type("Hola Python")) #tipo 'str'
+print(type(5)) #tipo 'int'
+print(type(2.5)) #tipo 'float'
+print(type(True)) #tipo 'bool'
+print(type(1 + 1j)) #tipo 'complex'
