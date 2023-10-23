a7f7572b0794f3949057df0b303c79b9b709e53f
Jeison Rico <jeison.rico@utp.edu.co>
2023-10-23 18:16:44 -0500

    Aprendiendo variables

---
+++ 01_Variables.py
@@ -0,0 +1,26 @@
+# Algunas variables
+cadena_texto = "Hola Python"
+print(cadena_texto)
+
+numero = 5
+print(numero)
+
+booleana = True
+print(booleana)
+
+#print puede recibir mas de un elemento, asi:
+print(cadena_texto, numero, booleana)
+print("Tengo el numero:", numero)
+
+#Tambien podemos cambiar el tipo de dato, asi:
+numero_a_cadena = str(numero) #Pasamos la varible que queremos convertir
+print(type(numero_a_cadena))
+
+#Ya que Python es debilmente tipado, tambien se puede cambiar el tipo asi:
+numero = "Jeison" #aunque numero era de tipo int, ahora la he cambiado a tipo str
+
+#podemos saber la longitud con la siguiente funcion del sistema
+print(len(cadena_texto))
+
+#Tambien podemos declarar variables en una sola linea (No abusar de esto)
+nombre, apellido, edad = "Jeison", "Rico", 21
